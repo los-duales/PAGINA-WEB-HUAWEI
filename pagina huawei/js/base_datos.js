74e4888b205256e4a56edde4aa0bdca3275a273c
Ángel Castillo <angeljrc12345@gmail.com>
2022-05-14 15:46:34 -0500

    info

--- pagina huawei/js/base_datos.js
+++ pagina huawei/js/base_datos.js
@@ -1,13 +1,13 @@
 const contenedor = document.getElementById('portfolio')
 
 const url = "https://raw.githubusercontent.com/los-duales/API/main/API/API.json"
-const uri = "https://raw.githubusercontent.com/los-duales/API/main/API/API.json"
+const uri = "https://raw.githubusercontent.com/los-duales/API/main/API/"
 fetch(url).then(function(response){
 	return response.json();
 }).then(function(datos){
 	console.log(datos);
 	for (let i = 0; i <datos.length; i++)
 
-        contenedor.innerHTML += "<div class='portfolio'><h3>"+ datos[i]["Nombre"] + "<h3></div";
+        contenedor.innerHTML += "<div>" + datos[i]["info"] + "</div>";
 	
 })
